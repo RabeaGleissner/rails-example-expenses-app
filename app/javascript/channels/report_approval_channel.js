06f6a3fb756d3c9bcbab3d7f657db30903b9fd01
Rabea Gleissner <rabeagleissner@gmail.com>
2021-12-13 19:38:05 +0800

    Update approval status when message received through websocket

--- app/javascript/channels/report_approval_channel.js
+++ app/javascript/channels/report_approval_channel.js
@@ -1,16 +1,26 @@
 import consumer from "./consumer";
 
 const initReportApprovalCable = () => {
-  console.log("working!");
+  const approvalContainers = document.querySelectorAll(".approval-container");
 
-  consumer.subscriptions.create(
-    { channel: "ReportApprovalChannel" },
-    {
-      received(data) {
-        console.log(data);
-      },
-    }
-  );
+  if (approvalContainers) {
+    consumer.subscriptions.create(
+      { channel: "ReportApprovalChannel" },
+      {
+        received(data) {
+          const approvedId = data.split(" ")[1];
+
+          const elementToChange = [...approvalContainers].find((container) => {
+            return container.dataset.expenseReportApprovalId === approvedId;
+          });
+
+          if (elementToChange) {
+            elementToChange.innerText = "YES";
+          }
+        },
+      }
+    );
+  }
 };
 
 export { initReportApprovalCable };
